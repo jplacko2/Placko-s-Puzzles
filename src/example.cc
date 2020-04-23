1c8bfe505960726a47e52bde5987de37afad5afa
jplacko2 <jplacko2@illinois.edu>
2020-04-23 13:09:21 -0500

    added imgui library, deleted code for libusb and bspline

--- src/example.cc
+++ src/example.cc
@@ -1,8 +1,14 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
-#include <mylibrary/example.h>
-#include <libusb>
+#include "mylibrary/example.h"
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "CinderImGui.h"
 
 namespace mylibrary {
-
+void setup() {
+  ui::initialize();
+}
 }  // namespace mylibrary
